98235f6b0cec69908e90cea74499f9caa8083d73
Diok4 <webdeveloperpath21@gmail.com>
2025-12-05 09:19:18 +0500

    fixed architechture bug

--- widgets/chat/chat-output/index.tsx
+++ widgets/chat/chat-output/index.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { useState } from "react";
+
 export const ChatOutput = () => {
-  return <></>;
+  const [post, isPost] = useState(false);
+
+  return (
+    <div>
+      {post ? <div className="">Text</div> : <div className="">No Text</div>}
+    </div>
+  );
 };
