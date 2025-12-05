232be4c459d3dce2cf04789907e4e6cc662fae7b
Diok4 <diokworkaet@gmail.com>
2025-12-05 12:36:04 +0500

    added auth ui

---
+++ src/features/auth/login/ui/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import { Input } from "@/src/shared/input";
+
+interface IForm {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export const LoginForm = () => {
+  const [formData, setFormData] = useState<IForm>({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  return (
+    <form className="gap-3 flex flex-col">
+      <Input
+        label="email"
+        name="email"
+        type="email"
+        placeholder="email@example"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <Input
+        label="password"
+        name="password"
+        type="password"
+        placeholder="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button className="bg-blue-900 w-full py-2 rounded-xl text-white hover:bg-blue-800 transition-all cursor-pointer">
+        Login
+      </button>
+    </form>
+  );
+};
