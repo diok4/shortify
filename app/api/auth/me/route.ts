44437abf7f12807c8cfd783eefcd861c4ff3b1d9
Diok4 <webdeveloperpath21@gmail.com>
2025-12-07 04:12:44 +0500

    added api getMe logic named me

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import prisma from "@/src/shared/lib/prisma";
+import { verifyToken } from "@/src/shared/lib/jwt";
+
+export const GET = async () => {
+  try {
+    const token = (await cookies()).get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json({ user: null });
+    }
+
+    const payload = verifyToken(token) as { id: string };
+
+    const user = await prisma.user.findUnique({
+      where: { id: payload.id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        avatarUrl: true,
+      },
+    });
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    return NextResponse.json({ user: null });
+  }
+};
